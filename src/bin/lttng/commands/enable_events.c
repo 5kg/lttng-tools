ee479287ba87e379cdd51384b7a6505ae7aaebdd
Zifei Tong <soariez@gmail.com>
2013-08-14 12:48:32 +0800

    Revert "Check error command"
    This reverts commit 6478e00c8b86e95dca368ffba325f0ea4cb8242f.
    
    Revert "Return value should be CMD_ERROR"
    
    This reverts commit f63727369efa584d6e151ce667272b437509dabf.
    
    Revert "Update usage() of enable_event to match manpage"
    
    This reverts commit aba75ebbadb331c04706970e4283d3283a4174ad.
    
    Revert "Update .gitignore"
    
    This reverts commit 4a648b24d5ac5eeaefb8c8dff8ef5a0e4869a477.
    
    Revert "Extend enable-event command for dynamically instrumented function probe."
    
    This reverts commit 6a0bf8a62c0f8c4b30c92cb7c113a3db4cf44400.

--- src/bin/lttng/commands/enable_events.c
+++ src/bin/lttng/commands/enable_events.c
@@ -42,10 +42,10 @@ static char *opt_function;
 static char *opt_function_entry_symbol;
 static char *opt_channel_name;
 static char *opt_filter;
-static pid_t opt_pid;
 #if 0
 /* Not implemented yet */
 static char *opt_cmd_name;
+static pid_t opt_pid;
 #endif
 
 enum {
@@ -72,7 +72,6 @@ static struct poptOption long_options[] = {
 	{"channel",        'c', POPT_ARG_STRING, &opt_channel_name, 0, 0, 0},
 	{"kernel",         'k', POPT_ARG_VAL, &opt_kernel, 1, 0, 0},
 	{"userspace",      'u', POPT_ARG_NONE, 0, OPT_USERSPACE, 0, 0},
-	{"pid",            'p', POPT_ARG_INT, &opt_pid, 0, 0, 0},
 	{"tracepoint",     0,   POPT_ARG_NONE, 0, OPT_TRACEPOINT, 0, 0},
 	{"probe",          0,   POPT_ARG_STRING, &opt_probe, OPT_PROBE, 0, 0},
 	{"function",       0,   POPT_ARG_STRING, &opt_function, OPT_FUNCTION, 0, 0},
@@ -101,12 +100,11 @@ static void usage(FILE *ofp)
 	fprintf(ofp, "Options:\n");
 	fprintf(ofp, "  -h, --help               Show this help\n");
 	fprintf(ofp, "      --list-options       Simple listing of options\n");
-	fprintf(ofp, "  -s, --session NAME       Apply on session name\n");
-	fprintf(ofp, "  -c, --channel NAME       Apply on channel name\n");
+	fprintf(ofp, "  -s, --session NAME       Apply to session name\n");
+	fprintf(ofp, "  -c, --channel NAME       Apply to this channel\n");
 	fprintf(ofp, "  -a, --all                Enable all tracepoints and syscalls\n");
 	fprintf(ofp, "  -k, --kernel             Apply for the kernel tracer\n");
-	fprintf(ofp, "  -u, --userspace          Apply for the user-space tracer\n");
-	fprintf(ofp, "  -p, --pid                Apply to specific process\n");
+	fprintf(ofp, "  -u, --userspace          Apply to the user-space tracer\n");
 	fprintf(ofp, "\n");
 	fprintf(ofp, "Event options:\n");
 	fprintf(ofp, "    --tracepoint           Tracepoint event (default)\n");
@@ -511,7 +509,7 @@ static int enable_events(char *session_name)
 				ret = parse_probe_opts(&ev, opt_function);
 				if (ret < 0) {
 					ERR("Unable to parse function probe options");
-					ret = CMD_ERROR;
+					ret = 0;
 					goto error;
 				}
 				break;
@@ -537,35 +535,28 @@ static int enable_events(char *session_name)
 			/* kernel loglevels not implemented */
 			ev.loglevel_type = LTTNG_EVENT_LOGLEVEL_ALL;
 		} else if (opt_userspace) {		/* User-space tracer action */
+#if 0
+			if (opt_cmd_name != NULL || opt_pid) {
+				MSG("Only supporting tracing all UST processes (-u) for now.");
+				ret = CMD_UNDEFINED;
+				goto error;
+			}
+#endif
+
 			DBG("Enabling UST event %s for channel %s, loglevel %s", event_name,
 					print_channel_name(channel_name), opt_loglevel ? : "<all>");
 
 			switch (opt_event_type) {
 			case LTTNG_EVENT_ALL:	/* Default behavior is tracepoint */
-				ev.type = LTTNG_EVENT_TRACEPOINT;
 				/* Fall-through */
 			case LTTNG_EVENT_TRACEPOINT:
-				if (opt_pid) {
-					MSG("Tracing specific process is not available for static compiled tracepoint");
-					ret = CMD_UNDEFINED;
-					goto error;
-				}
-				break;
-			case LTTNG_EVENT_FUNCTION:
-				if (!opt_pid) {
-					ERR("Process ID is required for user-space function probe");
-					ret = CMD_UNDEFINED;
-					goto error;
-				}
-				ev.pid = opt_pid;
-				ret = parse_probe_opts(&ev, opt_function);
-				if (ret < 0) {
-					ERR("Unable to parse function probe options");
-					ret = CMD_ERROR;
-					goto error;
-				}
+				/* Copy name and type of the event */
+				ev.type = LTTNG_EVENT_TRACEPOINT;
+				strncpy(ev.name, event_name, LTTNG_SYMBOL_NAME_LEN);
+				ev.name[LTTNG_SYMBOL_NAME_LEN - 1] = '\0';
 				break;
 			case LTTNG_EVENT_PROBE:
+			case LTTNG_EVENT_FUNCTION:
 			case LTTNG_EVENT_FUNCTION_ENTRY:
 			case LTTNG_EVENT_SYSCALL:
 			default:
