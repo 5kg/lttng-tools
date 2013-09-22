1618e34a19da5d296f9804a21287d36aa10f83b2
Zifei Tong <soariez@gmail.com>
2013-09-23 01:05:11 +0800

    Finish dynamic instrument tests

--- tests/regression/ust/dynamic-instrument/demo.c
+++ tests/regression/ust/dynamic-instrument/demo.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 
-void foo() {
-    usleep(1);
+void foo(void) {
+	usleep(1);
 }
 
 int main()
 {
-    int i;
-    for (i = 0; i < 10; ++i)
-        foo();
-    return 0;
+	int i;
+	/* Do not let gcc optimize foo out */
+	void (*call_foo)(void) = foo;
+
+	for (i = 0; i < 10; i++)
+		call_foo();
+	return 0;
 }
