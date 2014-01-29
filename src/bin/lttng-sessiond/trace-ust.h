028378c515245fb62bc2318e6e2a6fed5bea8ab6
Zifei Tong <soariez@gmail.com>
2014-01-30 00:09:38 +1100

    Handle instrument target in event_ust_enable_tracepoint()
    Add instrument target data to the prototype of trace_ust_create_event()
    and trace_ust_find_event().
    
    Update the test_ust_data unit test with the new prototype.

--- src/bin/lttng-sessiond/trace-ust.h
+++ src/bin/lttng-sessiond/trace-ust.h
@@ -154,7 +154,8 @@ int trace_ust_ht_match_event_by_name(struct cds_lfht_node *node,
  */
 struct ltt_ust_event *trace_ust_find_event(struct lttng_ht *ht,
 		char *name, struct lttng_filter_bytecode *filter, int loglevel,
-		struct lttng_event_exclusion *exclusion);
+		struct lttng_event_exclusion *exclusion,
+		struct lttng_event_target *target);
 struct ltt_ust_channel *trace_ust_find_channel_by_name(struct lttng_ht *ht,
 		char *name);
 
@@ -166,7 +167,8 @@ struct ltt_ust_channel *trace_ust_create_channel(struct lttng_channel *attr);
 struct ltt_ust_event *trace_ust_create_event(struct lttng_event *ev,
 		char *filter_expression,
 		struct lttng_filter_bytecode *filter,
-		struct lttng_event_exclusion *exclusion);
+		struct lttng_event_exclusion *exclusion,
+		struct lttng_event_target *target);
 struct ltt_ust_context *trace_ust_create_context(
 		struct lttng_event_context *ctx);
 void trace_ust_delete_channel(struct lttng_ht *ht,
@@ -213,7 +215,8 @@ static inline
 struct ltt_ust_event *trace_ust_create_event(struct lttng_event *ev,
 		const char *filter_expression,
 		struct lttng_filter_bytecode *filter,
-		struct lttng_event_exclusion *exclusion)
+		struct lttng_event_exclusion *exclusion,
+		struct lttng_event_target *target)
 {
 	return NULL;
 }
@@ -239,7 +242,8 @@ struct ltt_ust_context *trace_ust_create_context(
 }
 static inline struct ltt_ust_event *trace_ust_find_event(struct lttng_ht *ht,
 		char *name, struct lttng_filter_bytecode *filter, int loglevel,
-		struct lttng_event_exclusion *exclusion)
+		struct lttng_event_exclusion *exclusion,
+		struct lttng_event_target *target)
 {
 	return NULL;
 }
